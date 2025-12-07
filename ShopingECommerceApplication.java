5f5ec538979e77b5391769cef19aa55a1d9b6a0e
Harish kumar S <youthicon13032003@gmail.com>
2025-12-07 21:59:28 +0530

    Add files via upload

---
+++ ShopingECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.hari.shoping.e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingECommerceApplication.class, args);
+	}
+
+}

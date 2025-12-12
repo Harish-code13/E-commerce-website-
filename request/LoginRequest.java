590be005650c0469c20a43e4fff41dce08e7b272
Harish kumar S <youthicon13032003@gmail.com>
2025-12-12 21:44:45 +0530

    Add files via upload

---
+++ request/LoginRequest.java
@@ -0,0 +1,32 @@
+package com.shop.request;
+
+public class LoginRequest {
+
+   private String email;
+   private String password;
+
+   public LoginRequest(){
+
+   }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
